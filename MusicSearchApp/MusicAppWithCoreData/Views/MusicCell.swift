ab4c9d51f4c8c1e744932267a3d973c9874a1835
Zizi_Kim <rlarjsdn3@naver.com>
2023-08-18 12:43:45 +0900

    테이블 Cell 작동을 위한 코드 구현

--- MusicSearchApp/MusicAppWithCoreData/Views/MusicCell.swift
+++ MusicSearchApp/MusicAppWithCoreData/Views/MusicCell.swift
@@ -9,15 +9,91 @@ import UIKit
 
 class MusicCell: UITableViewCell {
 
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    // 이 프로퍼티를 통해 Music 객체를 넘겨주게 되면
+    // 프로퍼티 옵저버에 의해 셀의 각 항목에 데이터를 넣어줌
+    var music: Music? {
+        didSet {
+            configureUIWithData()
+        }
+    }
+    
+    // (커스텀 델리게이트를 대신해) 클로저를 실행해 뷰 컨트롤러에서 필요한 작업을 수행함
+    var saveButtonTapped: ((MusicCell, Bool) -> Void) = { (sender, pressed) in }
+    
+    // 셀이 재사용되기 전에 호출되는 메서드
+    override func prepareForReuse() {
+        self.prepareForReuse()
+        // 스크롤을 해서 셀이 재사용될 때, 이미지가 바뀌는 현상을 없애기 위함
+        self.mainImageView.image = nil
+    }
+    
+    // 셀이 인터페이스 빌더에서 불러올 때 호출되는 메서드 (한번만 호출됨)
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.mainImageView.contentMode = .scaleToFill
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
+    func configureUIWithData() {
+        // Music 객체를 옵셔널 바인딩
+        guard let music = music else { return }
+        loadImage(with: music.imageUrl)
+        songNameLabel.text = music.songName
+        artistNameLabel.text = music.artistName
+        albumNameLabel.text = music.albumName
+        releaseDateLabel.text = music.releaseDateString
+        setButtonStatus()
+    }
+    
+    func loadImage(with imageUrl: String?) {
+        // url을 옵셔널 바인딩하고, URL구조체로 초기화
+        guard let urlString = imageUrl, let url = URL(string: urlString) else {
+            return
+        }
+        // 데이터를 비동기적으로 다운로드 받기
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url) else { return }
+            // UI를 업데이트하는 일은 메인 쓰레드에서 하기
+            DispatchQueue.main.async {
+                self.mainImageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    func setButtonStatus() {
+        // Music 객체의 isSaved 프로퍼티 옵셔널 바인딩
+        guard let isSaved = music?.isSaved else {
+            return
+        }
+        // 찜한 음악인 경우
+        if isSaved {
+            saveButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        // 찜한 음악이 아닌 경우
+        } else {
+            saveButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        }
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        // Music 객체의 isSaved 프로퍼티 옵셔널 바인딩
+        guard let isSaved = music?.isSaved else {
+            return
+        }
+        // 뷰 컨트롤러에서 전달 받은 클로저를 실행함
+        saveButtonTapped(self, isSaved)
+        // 다시 찜한 음악 여부를 검사
+        setButtonStatus()
+    }
 }
--- MusicSearchApp/MusicAppWithCoreData/Views/SavedMusicCell.swift
+++ MusicSearchApp/MusicAppWithCoreData/Views/SavedMusicCell.swift
@@ -9,15 +9,96 @@ import UIKit
 
 class SavedMusicCell: UITableViewCell {
 
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var saveDateLabel: UILabel!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    // 이 프로퍼티를 통해 MusicSaved 객체를 넘겨주게 되면
+    // 프로퍼티 옵저버에 의해 셀의 각 항목에 데이터를 넣어줌
+    var musicSaved: MusicSaved? {
+        didSet {
+            configureUIwithData()
+        }
+    }
+    
+    // (커스텀 델리게이트를 대신해) 클로저를 실행해 뷰 컨트롤러에서 필요한 작업을 수행함
+    var updateButtonTapped: ((SavedMusicCell) -> Void) = { (sender) in }
+    
+    // (커스텀 델리게이트를 대신해) 클로저를 실행해 뷰 컨트롤러에서 필요한 작업을 수행함
+    var saveButtonTapped: ((SavedMusicCell) -> Void) = { (sender) in }
+    
+    // 셀이 재사용되기 전에 호출되는 메서드
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // 스크롤을 해서 셀이 재사용될 때, 이미지가 바뀌는 현상을 없애기 위함
+        self.mainImageView.image = nil
+    }
+    
+    // 셀이 인터페이스 빌더에서 불러올 때 호출되는 메서드 (한번만 호출됨)
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // 뷰의 레이아웃을 바꾸는 작업은 주로 이 메서드에서 수행함
+        setupUpdateButton()
+    }
+    
+    func setupUpdateButton() {
+        updateButton.layer.masksToBounds = true
+        updateButton.layer.cornerRadius = 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
+    func configureUIwithData() {
+        // MusicSaved 객체를 옵셔널 바인딩
+        guard let musicSaved = musicSaved else { return }
+        loadImage(with: musicSaved.imageUrl)
+        songNameLabel.text = musicSaved.songName
+        artistNameLabel.text = musicSaved.artistName
+        albumNameLabel.text = musicSaved.albumName
+        releaseDateLabel.text = musicSaved.releaseDate
+        saveDateLabel.text = "Saved Date: \(musicSaved.savedDateString ?? "")"
+        messageLabel.text = musicSaved.myMessage
+        setButtonStatus()
+    }
+    
+    func loadImage(with imageUrl: String?) {
+        // url을 옵셔널 바인딩하고, URL구조체로 초기화
+        guard let urlString = imageUrl, let url = URL(string: urlString) else {
+            return
+        }
+        // 데이터를 비동기적으로 다운로드 받기
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url) else { return }
+            // UI를 업데이트하는 일은 메인 쓰레드에서 하기
+            DispatchQueue.main.async {
+                self.mainImageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    func setButtonStatus() {
+        saveButton.setImage(UIImage(systemName: "heart"), for: .normal)
+    }
+    
+    @IBAction func updateButtonPressed(_ sender: UIButton) {
+        // 뷰 컨트롤러에서 전달 받은 클로저 실행
+        updateButtonTapped(self)
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        // 뷰 컨트롤러에서 전달 받은 클로저 실행
+        saveButtonTapped(self)
+    }
 }
