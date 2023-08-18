627ca6ad1ae91aa936414ab3340baef3b54e3b1e
Zizi_Kim <rlarjsdn3@naver.com>
2023-08-18 10:57:59 +0900

    테이블 Cell UI 구현

---
+++ MusicSearchApp/MusicAppWithCoreData/Views/SavedMusicCell.swift
@@ -0,0 +1,23 @@
+//
+//  SavedMusicCell.swift
+//  MusicAppWithCoreData
+//
+//  Created by 김건우 on 2023/08/18.
+//
+
+import UIKit
+
+class SavedMusicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
